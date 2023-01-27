b90fd57ae12fb567ce281adaf2596e25dac65148
Wayne Yong <ymw0331@gmail.com>
2023-01-27 16:09:22 +0800

    add reverse string solution

--- algocast/exercises/reversestring/index.js
+++ algocast/exercises/reversestring/index.js
@@ -2,10 +2,55 @@
 // Given a string, return a new string with the reversed
 // order of characters
 // --- Examples
-//   reverse('apple') === 'leppa'
+//   reverse( 'apple' ) === 'leppa';
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+
+
+
+// Solution #3
+function reverse ( str )
+{
+  // 1. take str, turn it into array by calling split function, then use reduce helper (reduce take all diff values within array and condense them all down to one singular value)
+  return str.split( "" ).reduce( ( rev, char ) => char + rev, '' );
+}
+
+
+// Solution #2
+// function reverse ( str )
+// {
+//   // 1. Create an empty string called 'reversed'
+//   let reversed = '';
+
+//   // 2. for each charater in the provided string, take the character and add it to the start of the reversed
+//   // character == temp var, str == iterable object, iterate tru each character of str one by one, set each character equal to temp 'charater'
+//   for ( let character of str )
+//   {
+//     reversed = character + reversed;
+//   }
+
+//   // 3. return the variable 'reversed'
+//   return reversed;
+// }
+
+
+
+
+// Solution #1 
+// function reverse ( str )
+// {
+
+//   return str.split( "" ).reverse().join( "" );
+
+//   // 1. turn str into an array
+//   const arr = str.split( '' );
+
+//   // 2. call 'reverse' method on the array
+//   arr.reverse();
+
+//   // 3. join the array back into a string
+//   return arr.join( '' );
+// }
 
 module.exports = reverse;
