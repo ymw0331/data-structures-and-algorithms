6166654aea85e156a480868d72fcee72b17e36ee
Wayne Yong <ymw0331@gmail.com>
2023-02-28 17:20:24 +0800

    add solution of vowels

--- algocast/exercises/011-vowels/index.js
+++ algocast/exercises/011-vowels/index.js
@@ -7,6 +7,27 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+// Solution #2 (regex solution)
+// function vowels( str ) {
+//   const matches = str.match( /[aeiou]/gi ) //g - dont stop at first search , i - insensitive
+//   return matches ? matches.length : 0 // true: array with length, false: 0
+
+// }
+
+
+// Solution #1 (iterative solution)
+function vowels( str ) {
+
+  let count = 0;
+  const vowelChecker = [ 'a', 'e', 'i', 'o', 'u' ]
+
+  for ( let char of str.toLowerCase() ) {
+    if ( vowelChecker.includes( char ) ) {
+      count++
+    }
+  }
+  return count
+}
+
 
 module.exports = vowels;
